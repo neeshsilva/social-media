003093ec96bbfa15b01394c4e930955fd714c086
neeshsilva <neeshsilva1@gmail.com>
2020-07-23 14:02:19 +0530

    social media icons share buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShareButtonsComponent } from './share-buttons/share-buttons.component';
+import {DeviceDetectorModule} from 'ngx-device-detector';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShareButtonsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DeviceDetectorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/share-buttons/share-buttons.component.ts
@@ -0,0 +1,54 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {DeviceDetectorService} from 'ngx-device-detector';
+
+@Component({
+  selector: 'app-share-buttons',
+  templateUrl: './share-buttons.component.html',
+  styleUrls: ['./share-buttons.component.css']
+})
+export class ShareButtonsComponent implements OnInit {
+
+  url = 'http://www.google.com/';
+  pageName = '';
+
+  constructor(private router: Router, private deviceService: DeviceDetectorService) {
+  }
+
+  ngOnInit(): void {
+    // this.url = 'https://www.youtube.com/watch?v=5kAQUNFZJXY';
+    this.url = window.location.href;
+    this.pageName = this.router.url;
+    // this.copyUrl.value = this.pageUrl;
+    console.log('PAGE URL ', this.router.url);
+    console.log('PAGE URL2 ', window.location.href);
+  }
+
+  getFacebookUrl() {
+    return `https://www.facebook.com/sharer/sharer.php?u=${this.url}`;
+  }
+
+  getTwitterUrl() {
+    return `https://twitter.com/intent/tweet?source=tweetbutton&text=${this.pageName}&url=${this.url}`;
+  }
+
+  getTelegramUrl() {
+    return `https://t.me/share/url?url=${this.url}&text=${this.pageName}`;
+  }
+
+  getWhatsappUrl() {
+    return this.deviceService.isMobile() ? `whatsapp://send?text=${this.url}` : `https://api.whatsapp.com/send?text=${this.url}`;
+  }
+
+  getCopyUrl() {
+    const copyUrl = document.createElement('input');
+    this.url = window.location.href;
+    document.body.appendChild(copyUrl);
+    copyUrl.value = this.url;
+    copyUrl.select();
+    document.execCommand('copy');
+    document.body.removeChild(copyUrl);
+    alert('URL is copied to clipboard');
+  }
+
+}
